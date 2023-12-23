e5b05700406c278ad35c9465ee3f58d0b60c37d2
Alex <akotenko@prezna.com>
2023-12-23 16:03:34 -0500

    1912L_data: added unique constraint to email column, add Faker, improved entity for support validation flyway

--- src/main/java/com/kotenko/fullstack/customer/Customer.java
+++ src/main/java/com/kotenko/fullstack/customer/Customer.java
@@ -9,22 +9,29 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "customer", uniqueConstraints = {
+        @UniqueConstraint(
+                name = "customer_unique_email",
+                columnNames = "email"
+        )
+})
 public class Customer {
     @Id
     @SequenceGenerator(
-            name = "customer_id_sequence",
-            sequenceName = "customer_id_sequence"
+            name = "customer_id_seq",
+            sequenceName = "customer_id_seq",
+            allocationSize = 1
     )
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
-            generator = "customer_id_sequence"
+            generator = "customer_id_seq"
     )
     private Integer id;
     @Column(nullable = false)
     private String name;
     @Column(nullable = false)
     private String email;
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private Integer age;
 
     public Customer(String name, String email, Integer age) {
